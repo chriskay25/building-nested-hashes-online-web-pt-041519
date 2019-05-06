22b22c90024a4ed251d233a32e1c01f3322f1ddd
Chris Kay <chriskay25@gmail.com>
2019-05-06 19:57:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,7 +15,16 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [
-        
+        {
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"
+        },
+        {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+        }
         ]
    }, 
    :capulet => {
@@ -32,7 +41,18 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
+        ]
    }
   }
 
